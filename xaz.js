e1d124428b0f3c5aab9ee9660e40d8ea6eb4204f
aa168188 <sli010342@gmail.com>
2025-02-20 16:36:51 +0800

    Update xaz.js

--- xaz.js
+++ xaz.js
@@ -1,9 +1,9 @@
  
 
-var xz = "/xiazai/Lets-VIP.zip";
+var xz = "/xiazai/lets-kuailian.zip";
 
-//可以把   /xiazai/letspn-latest64.zip  这个改成下载地址；https://guoxiauzausj.oss-rg-china-mainland.aliyuncs.com/letspn-latest64.zip
-//可以把   https://guoxiauzausj.oss-rg-china-mainland.aliyuncs.com/letspn-latest64.zip  这个改成文件下载：/xiazai/kuailian.zip
+//可以把   /xiazai/letspn-latest64.zip  这个改成下载地址；https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/lets-kuailian.zip
+//可以把   https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/lets-kuailian.zip  这个改成文件下载：/xiazai/lets-kuailian.zip
 
 
 
