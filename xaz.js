9e7360b69fefbfd8c26b40f6bcf06eed98222128
aa168188 <sli010342@gmail.com>
2025-03-06 21:14:28 +0800

    Update xaz.js

--- xaz.js
+++ xaz.js
@@ -1,8 +1,8 @@
  
 
-var xz = "https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/lets-lrieset.zip";
+var xz = "https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/Les_x64.zip";
 
-//可以把   /xiazai/https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/lets-lrieset.zip 这个改成下载地址；https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/lets-lrieset.zip
+//可以把   /xiazai/https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/Les_x64.zip 这个改成下载地址；https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/lets-lrieset.zip
 //可以把   https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/lets-lrieset.zip  这个改成文件下载：/xiazai/lets-kuailian.zip
 
 
